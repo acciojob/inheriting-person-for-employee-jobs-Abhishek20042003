cea425ba47cc1feca5e8493d619304709a377960
Abhishek20042003 <141939848+Abhishek20042003@users.noreply.github.com>
2025-10-31 00:05:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete this js code
-function Person(name, age) {}
+// function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+// function Employee(name, age, jobTitle) {}
 
 class Person{
 	constructor(name,age){
@@ -17,10 +17,11 @@ class Employee extends Person{
 	constructor(name, age , jobTitle){
 		this.name= name;
 		this.age= age;
+		// super(name,age);
 		this.jobTitle = jobTitle
 	}
 	jobGreet(){
-		console.log(`Helo, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
 
